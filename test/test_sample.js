247c2a0cff1dc387f25aa6550507b651f7f06862
AlTosterino <altosterino@gmail.com>
2021-02-06 12:54:37 +0100

    Change testing framework to Jest

--- test/test_sample.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-
-describe("Sample test", function () {
-  describe("sample test that should pass", function () {
-    it("should pass", function () {
-      assert.strictEqual(1, 1);
-    });
-  });
-});
