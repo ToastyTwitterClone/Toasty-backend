19d17b905f4ad81074c5c9cfcf3cc3ea6491a9e0
AlTosterino <altosterino@gmail.com>
2021-02-15 19:27:44 +0100

    Add user repository implementation in memory

---
+++ lib/infrastructure/repositories/user/userRepositoryInMemory.ts
@@ -0,0 +1,38 @@
+import UserRepository from "../../../domain/user/userRpository";
+import { User } from "../../../domain/user/user";
+import RepositoryError from "../../../shared/exceptions/repository";
+
+export default class UserRepositoryInMemory implements UserRepository {
+  private _database: Array<User>;
+
+  constructor() {
+    this._database = [];
+  }
+
+  public async create(user: User): Promise<User> {
+    const alreadyRegistered = await this.getByEmail(user.email);
+    if (alreadyRegistered) {
+      throw new RepositoryError("User with this email already exists");
+    }
+    this._database.push(user);
+    return Promise.resolve(user);
+  }
+
+  public async getByEmail(email: string): Promise<User | null> {
+    for (const user of this._database) {
+      if (user.email === email) {
+        return Promise.resolve(user);
+      }
+    }
+    return Promise.resolve(null);
+  }
+
+  public async getById(user_id: string): Promise<User | null> {
+    for (const user of this._database) {
+      if (user.id === user_id) {
+        return Promise.resolve(user);
+      }
+    }
+    return Promise.resolve(null);
+  }
+}
